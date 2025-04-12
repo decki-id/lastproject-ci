0994eb3f03b402921934f03ad0187422fe194018
decki-id <d.herdiawan.s@gmail.com>
2025-04-13 00:13:16 +0700

    Change logo, rearrange logo size & position, refactor conditions for the active menu.

--- application/views/templates/agent_sidebar.php
+++ application/views/templates/agent_sidebar.php
@@ -1,22 +1,18 @@
 <div class="nav-left-sidebar sidebar-dark">
     <ul class="navbar-nav flex-column sidebar-nav-fixed">
-        <a href="<?= base_url('agent/'); ?>"><img src="<?= base_url('images/'); ?>logorevota.jpg" id="navbar-brand"></a>
+        <div id="navbar-brand">
+            <a href="<?= base_url('agent/'); ?>"><img src="<?= base_url('images/'); ?>logo-ci.png" width="60"></a>
+        </div>
         <li class="nav-item">
-            <?php if ($title == 'Tickets') : ?>
-                <a href="<?= base_url('agent/'); ?>" class="nav-link active"><i class="fas fa-fw fa-ticket-alt"></i>Tickets</a>
-            <?php else : ?>
-                <a href="<?= base_url('agent/'); ?>" class="nav-link"><i class="fas fa-fw fa-ticket-alt"></i>Tickets</a>
-            <?php endif; ?>
-            <?php if ($title == 'Lists') : ?>
-                <a href="<?= base_url('agent/lists_menu/'); ?>" class="nav-link active"><i class="fa fa-fw fa-user-circle"></i>Lists</a>
-            <?php else : ?>
-                <a href="<?= base_url('agent/lists_menu/'); ?>" class="nav-link"><i class="fa fa-fw fa-user-circle"></i>Lists</a>
-            <?php endif; ?>
-            <?php if ($title == 'Reports') : ?>
-                <a href="<?= base_url('agent/reports_menu/'); ?>" class="nav-link active"><i class="far fa-fw fa-chart-bar"></i>Reports</a>
-            <?php else : ?>
-                <a href="<?= base_url('agent/reports_menu/'); ?>" class="nav-link"><i class="far fa-fw fa-chart-bar"></i>Reports</a>
-            <?php endif; ?>
+            <a href="<?= base_url('agent/'); ?>" class="nav-link <?= $title == 'Tickets' ? 'active' : ''; ?>">
+                <i class="fas fa-fw fa-ticket-alt"></i>Tickets
+            </a>
+            <a href="<?= base_url('agent/lists_menu/'); ?>" class="nav-link <?= $title == 'Lists' ? 'active' : '' ?>">
+                <i class="fa fa-fw fa-user-circle"></i>Lists
+            </a>
+            <a href="<?= base_url('agent/reports_menu/'); ?>" class="nav-link <?= $title == 'Reports' ? 'active' : '' ?>">
+                <i class="far fa-fw fa-chart-bar"></i>Reports
+            </a>
         </li>
     </ul>
 </div>
