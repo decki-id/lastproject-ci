756b75eeb2b1e42b2feb71e40e047e4b29b6a281
decki-id <d.herdiawan.s@gmail.com>
2025-04-13 21:37:48 +0700

    Refactor indentations & rearrange tags.

--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-    	<link rel="icon" type="image/png" href="<?= base_url('images/'); ?>favicon.ico" />
-    	<title>Ticketing Support System</title>
-        <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?= base_url('assets/'); ?>libs/css/style-auth.css">
-    </head>
-<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>libs/css/style-auth.css">
+    <link rel="icon" type="image/png" href="<?= base_url('images/'); ?>favicon.ico" >
+    <title>Ticketing Support System</title>
+  </head>
