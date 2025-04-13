23275ade679ffa09e45c0b1e4ce52956493271f5
decki-id <d.herdiawan.s@gmail.com>
2025-04-13 21:36:46 +0700

    Refactor indentations.

--- application/helpers/access_helper.php
+++ application/helpers/access_helper.php
@@ -1,27 +1,16 @@
 <?php
-    function is_logged_in() {
-        $ci = get_instance();
-
-        if (!$ci->session->userdata('username')) {
-            $ci->session->set_flashdata('flash', 'You are not logged in!');
-
-            echo "<div class='flash-data-error' data-flashdata='<?= $ci->session->flashdata('flash'); ?>'></div>";
-            redirect('auth/');
-        } else {
-            $role_id = $ci->session->userdata('role_id');
-            $menu = $ci->uri->segment(1);
-
-            $queryMenu  = $ci->db->get_where('agent_menu', ['uri_segment' => $menu])->row_array();
-            $menu_id = $queryMenu['id'];
-
-            $userAccess = $ci->db->get_where('user_access_menu', [
-                'role_id' => $role_id, 
-                'menu_id' => $menu_id
-            ]);
-
-            if ($userAccess->num_rows() < 1) {
-                redirect('auth/blocked/');
-            }
-        }
+  function is_logged_in() {
+    $ci = get_instance();
+    if (!$ci->session->userdata('username')) {
+      $ci->session->set_flashdata('flash', 'You are not logged in!');
+      redirect('auth/');
+    } else {
+      $role_id = $ci->session->userdata('role_id');
+      $menu = $ci->uri->segment(1);
+      $queryMenu  = $ci->db->get_where('agent_menu', ['uri_segment' => $menu])->row_array();
+      $menu_id = $queryMenu['id'];
+      $userAccess = $ci->db->get_where('user_access_menu', ['role_id' => $role_id, 'menu_id' => $menu_id]);
+      if ($userAccess->num_rows() < 1) { redirect('auth/blocked/'); }
     }
+  }
 ?>
--- assets/libs/js/myscript.js
+++ assets/libs/js/myscript.js
@@ -1,40 +1,18 @@
 $('.custom-file-input').on('change', function () {
-	let fileName = $(this).val().split('\\').pop();
-	$(this).next('.custom-file-label').addClass("selected").html(fileName);
-});
+	let fileName = $(this).val().split('\\').pop()
+	$(this).next('.custom-file-label').addClass("selected").html(fileName)
+})
 
-function bacaGambar(input) {
+function readPicture(input) {
 	if (input.files && input.files[0]) {
-		var reader = new FileReader();
-
-		reader.onload = function (e) {
-			$('#image-preview').attr('src', e.target.result);
-		}
-
-		reader.readAsDataURL(input.files[0]);
+		let reader = new FileReader()
+		reader.onload = function (e) { $('#image-preview').attr('src', e.target.result) }
+		reader.readAsDataURL(input.files[0])
 	}
 }
+$(".custom-file-input").change(function () { readPicture(this) })
 
-$(".custom-file-input").change(function () {
-	bacaGambar(this);
-});
-
-const flashDataError = $('.flash-data-error').data('flashdata');
-
-if (flashDataError) {
-	Swal.fire({
-		title: flashDataError,
-		text: '',
-		icon: 'error'
-	});
-}
-
-const flashDataSuccess = $('.flash-data-success').data('flashdata');
-
-if (flashDataSuccess) {
-	Swal.fire({
-		title: flashDataSuccess,
-		text: '',
-		icon: 'success'
-	});
-}
+const flashDataError = $('.flash-data-error').data('flashdata')
+const flashDataSuccess = $('.flash-data-success').data('flashdata')
+if (flashDataError) { Swal.fire({ title: flashDataError, text: '', icon: 'error' }) }
+if (flashDataSuccess) { Swal.fire({ title: flashDataSuccess, text: '', icon: 'success' }) }
