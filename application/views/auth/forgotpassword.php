5c67f004cc71cd6af8892cd4626a72638cb5e46a
decki-id <d.herdiawan.s@gmail.com>
2025-04-22 23:14:22 +0700

    Improve Login page & refactor Forgot Password page.

--- application/views/auth/forgotpassword.php
+++ application/views/auth/forgotpassword.php
@@ -6,21 +6,23 @@
 	<?php $this->session->unset_userdata('flash-success') ?>
 <?php endif; ?>
 
-<div id="panel">
-	<div class="text-center" id="login-panel">
-		<img src="<?= base_url('images/'); ?>logo-ci.png" width="100" />
-		<h2>Forgot Password</h2>
-		<form method="post" action="<?= base_url('auth/forgotpassword/'); ?>">
-			<div class="form-group">
-				<label for="email">
-					<small>Enter your email address below and we will send you a link to reset your password.</small>
-				</label>
-				<input type="email" class="form-control" id="email" name="email" placeholder="Email address" required autofocus />
-			</div>
-			<div class="row">
-				<div class="col"><button type="submit" class="btn btn-primary">Reset My Password</button></div>
-				<div class="col"><a href="<?= base_url(); ?>" class="col btn btn-secondary">Back to Login Page</a></div>
-			</div>
-		</form>
+<div class="position-fixed w-100 h-100 d-flex justify-content-center align-items-center" id="panel">
+	<div class="card w-40 w-mod">
+		<div class="card-body text-center">
+			<img src="<?= base_url('images/'); ?>logo-ci.png" width="100" />
+			<h4 class="card-title mt-2 mb-1">Forgot Password</h4>
+			<form method="post" action="<?= base_url('auth/forgotpassword/'); ?>">
+				<div class="form-group">
+					<label for="email">
+						<small>Enter your email address below and we will send you a link to reset your password.</small>
+					</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="Email address" required autofocus />
+				</div>
+				<div class="row">
+					<div class="col col-mod"><button type="submit" class="btn btn-primary btn-block">Reset My Password</button></div>
+					<div class="col"><a href="<?= base_url(); ?>" class="col btn btn-secondary">Back to Login Page</a></div>
+				</div>
+			</form>
+		</div>
 	</div>
 </div>
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,7 +9,7 @@
 	<?php $this->session->unset_userdata('flash') ?>
 <?php endif; ?>
 
-<div id="panel">
+<!-- <div class="position-fixed w-100 h-100" id="panel">
 	<div id="login-panel">
 		<div class="text-center">
 			<img src="<?= base_url('images/'); ?>logo-ci.png" width="100" />
@@ -33,6 +33,42 @@
 		<hr>
 		<div class="text-center">
 			<a href="<?= base_url('auth/forgotpassword/'); ?>"><small>Forgot Password?</small></a>
+		</div>
+	</div>
+</div> -->
+
+<div class="position-fixed w-100 h-100 d-flex justify-content-center align-items-center" id="panel">
+	<div class="card w-50 w-mod">
+		<div class="card-body">
+			<div class="text-center">
+				<img class="text-center" src="<?= base_url('images/'); ?>logo-ci.png" width="100" />
+				<h4 class="card-title mt-2">Helpdesk Ticketing System</h4>
+			</div>
+			<hr>
+			<div class="container px-5 pb-2">
+				<form method="post" action="<?= base_url('auth/'); ?>">
+					<div class="form-group row pb-mod">
+						<label for="username" class="col-sm-4 col-form-label l-pt-mod">Username</label>
+						<div class="col-sm-8">
+							<input type="text" class="form-control" id="username" name="username" value="<?= set_value('username'); ?>" required autofocus>
+						</div>
+					</div>
+					<div class="form-group row pt-mod">
+						<label for="password" class="col-sm-4 col-form-label">Password</label>
+						<div class="col-sm-8">
+							<input type="password" class="form-control" id="password" name="password" required>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col col-mb-mod"><button type="submit" class="btn btn-primary btn-block">Log In</button></div>
+						<div class="col"><a href="<?= base_url(); ?>" class="col btn btn-success">Sign Up</a></div>
+					</div>
+				</form>
+			</div>
+			<hr>
+			<div class="text-center">
+				<a href="<?= base_url('auth/forgotpassword/'); ?>"><small>Forgot Password?</small></a>
+			</div>
 		</div>
 	</div>
 </div>
