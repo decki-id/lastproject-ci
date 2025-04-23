aa6db12bbbc613a764336c68100471e9b2f9fddc
decki-id <d.herdiawan.s@gmail.com>
2025-04-23 20:28:38 +0700

    Remove the old commented login card & remove unused 'class' attribute on the 'img' tag.

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,39 +9,11 @@
 	<?php $this->session->unset_userdata('flash') ?>
 <?php endif; ?>
 
-<!-- <div class="position-fixed w-100 h-100" id="panel">
-	<div id="login-panel">
-		<div class="text-center">
-			<img src="<?= base_url('images/'); ?>logo-ci.png" width="100" />
-			<h4>Helpdesk Ticketing System</h4>
-		</div>
-		<form method="post" action="<?= base_url('auth/'); ?>">
-			<div class="form-group row">
-				<label for="username" class="col-sm-2 col-form-label ml-1">Username</label>
-				<div class="col-sm-9 ml-4">
-					<input type="text" class="form-control" id="username" name="username" value="<?= set_value('username'); ?>" required autofocus>
-				</div>
-			</div>
-			<div class="form-group row">
-				<label for="password" class="col-sm-2 col-form-label ml-1">Password</label>
-				<div class="col-sm-9 ml-4">
-					<input type="password" class="form-control" id="password" name="password" required>
-				</div>
-			</div>
-			<button type="submit" class="btn btn-primary btn-block">Log In</button>
-		</form>
-		<hr>
-		<div class="text-center">
-			<a href="<?= base_url('auth/forgotpassword/'); ?>"><small>Forgot Password?</small></a>
-		</div>
-	</div>
-</div> -->
-
 <div class="position-fixed w-100 h-100 d-flex justify-content-center align-items-center" id="panel">
 	<div class="card w-50 w-mod">
 		<div class="card-body">
 			<div class="text-center">
-				<img class="text-center" src="<?= base_url('images/'); ?>logo-ci.png" width="100" />
+				<img src="<?= base_url('images/'); ?>logo-ci.png" width="100">
 				<h4 class="card-title mt-2">Helpdesk Ticketing System</h4>
 			</div>
 			<hr>
