3d83afc80eb5fdd435447f8087b892d9543c5820
decki-id <d.herdiawan.s@gmail.com>
2025-04-23 20:24:11 +0700

    Refactor Change Password page.

--- application/views/auth/changepassword.php
+++ application/views/auth/changepassword.php
@@ -1,20 +1,24 @@
-<div id="panel">
-	<div id="login-panel">
-		<div class="text-center"><img src="<?= base_url('images/'); ?>logo-ci.png" width="100" /></div>
-		<h2 class="text-center">Change Password for:</h2>
-		<form method="post" action="<?= base_url('auth/changepassword/'); ?>">
-			<div class="form-group"><h5 class="text-center"><?= $this->session->userdata('reset_email'); ?></h5></div>
-			<div class="form-group">
-				<input type="password" class="form-control" id="password1" name="password1" placeholder="Enter your new password" required autofocus />
-				<?= form_error('password1', '<small class="text-danger pl-1">', '</small>'); ?>
+<div class="position-fixed w-100 h-100 d-flex justify-content-center align-items-center" id="panel">
+	<div class="card w-25 w-cp-mod">
+		<div class="card-body">
+			<div class="text-center">
+				<img src="<?= base_url('images/'); ?>logo-ci.png" width="100">
+				<h4 class="card-title mt-2 mb-1">Change Password for:</h4>
 			</div>
-			<div class="form-group">
-				<input type="password" class="form-control" id="password2" name="password2" placeholder="Repeat password" required />
-				<?= form_error('password2', '<small class="text-danger pl-1">', '</small>'); ?>
-			</div>
-			<div class="form-group"><button type="submit" class="btn btn-primary btn-block">Change My Password</button></div>
-		</form>
-		<hr>
-		<div class="text-center"><a href="<?= base_url(); ?>"><small>&larr; Back to Login Page</small></a></div>
+			<form method="post" action="<?= base_url('auth/changepassword/'); ?>">
+				<div class="form-group"><h5 class="text-center"><?= $this->session->userdata('reset_email'); ?></h5></div>
+				<div class="form-group">
+					<input type="password" class="form-control" id="password1" name="password1" placeholder="Enter your new password" required autofocus>
+					<?= form_error('password1', '<small class="text-danger pl-1">', '</small>'); ?>
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" id="password2" name="password2" placeholder="Repeat password" required>
+					<?= form_error('password2', '<small class="text-danger pl-1">', '</small>'); ?>
+				</div>
+				<div class="form-group"><button type="submit" class="btn btn-primary btn-block">Change My Password</button></div>
+			</form>
+			<hr>
+			<div class="text-center"><a href="<?= base_url(); ?>"><small>&larr; Back to Login Page</small></a></div>
+		</div>
 	</div>
 </div>
